19dc127488bb4110c599812b31412414a171a797
Daniel O'Shaughnessy <doshaughnessy@salesforce.com>
2021-07-02 14:15:24 +0100

    dont load dotenv in prod

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
-require('dotenv').config()
+try {
+  require('dotenv').config()
+} catch (e) { 
+  console.log("SKIP env")
+}
 
 try {
   // `who-to-greet` input defined in action metadata file
